4e9735a54d8b0a45973205c5d494a3b88ae3ba6a
Saint <sainthxd@gmail.com>
2016-04-06 14:21:24 +0800

    add more

--- fabfile.py
+++ fabfile.py
@@ -6,34 +6,45 @@ import ConfigParser
 
 cf=ConfigParser.ConfigParser()
 cf.read('passwd.ini')
+activeSession=cf.get('global','activeSession')
 #env.hosts=['localhost']
 env.disable_known_hosts=True
 #env.key_filename=['~/Desktop/hxd.pem']
 env.user=cf.get('global','root')
 env.password = cf.get('global','passwd')
-env.roledefs = {
-    'server': {
-        'hosts': ['192.168.130.3', '192.168.130.5', '192.168.130.6', '192.168.130.7', '192.168.130.8',
-        '192.168.130.9', '192.168.130.11', '192.168.130.12', '192.168.130.13', '192.168.130.15',
-        '192.168.130.16', '192.168.130.17', '192.168.130.18', '192.168.130.19', '192.168.130.20'],
-    'ips': [ 's3', 's5', 's6', 's7', 's8',
-       's9', 's11', 's12', 's13', 's15',
-       's16', 's17', 's18', 's19', 's20']
-    },
-    'client': {
-        'hosts': ['192.168.130.2'],
-        'ips': ['s2']
-    }
-}
-@roles('client')
-def createClientUser():
-    createUser(cf.get('client','newuser'),cf.get('client','passwd'))
 
-@roles('server')
-def createServerUser():
-    createUser(cf.get('server','newuser'),cf.get('server','passwd'))
+def lambdastrp(x):
+    return x.strip()
+env.hosts=map(lambdastrp, cf.get(activeSession,'hosts').split(','))
+env.hostnames=map(lambdastrp,cf.get(activeSession,'hostnames').split(','))
+env.hostmap={}
 
-def createUser(name, passwd):
+i=0
+while i<len(env.hosts):
+    env.hostmap[env.hosts[i]]=env.hostnames[i]
+    i=i+1
+
+#env.roledefs = {
+#    'server': {
+#        'hosts': ['192.168.130.3', '192.168.130.5', '192.168.130.6', '192.168.130.7', '192.168.130.8',
+#        '192.168.130.9', '192.168.130.11', '192.168.130.12', '192.168.130.13', '192.168.130.15',
+#        '192.168.130.16', '192.168.130.17', '192.168.130.18', '192.168.130.19', '192.168.130.20'],
+#    'ips': [ 's3', 's5', 's6', 's7', 's8',
+#       's9', 's11', 's12', 's13', 's15',
+#       's16', 's17', 's18', 's19', 's20']
+#    },
+#    'client': {
+#        'hosts': ['192.168.130.2'],
+#        'ips': ['s2']
+#    },
+#    'test':{
+#        'hosts': ['localhost']
+#    }
+#}
+user=cf.get(activeSession,'newuser')
+passwd=cf.get(activeSession,'passwd')
+#批量创建用户
+def createUser():
     with settings(warn_only=True):
         with settings(prompts={
             '输入新的 UNIX 密码： ': passwd,
@@ -53,9 +64,52 @@ def createUser(name, passwd):
             '这些信息是否正确？ [Y/n] ': 'Y',
             'Is the information correct? [Y/n] ': 'Y',
             }):
-            sudo('adduser '+name ,pty=True, combine_stderr=True)
+            sudo('adduser '+user ,pty=True, combine_stderr=True)
+#批量删除用户
+def removeUser():
+    sudo('deluser '+ user, pty=True, combine_stderr=True)
+
+#修改hosts
+def addIntoHostFile():
+    sudo('echo "' + generateHosts() + '" >> /etc/hosts')
+
+def generateHosts():
+    hosts=''
+    i=0
+    size=len(env.hosts)
+    while i<size:
+        hosts = hosts + '\n' + env.hosts[i] + '\t' + env.hostnames[i]
+        i=i+1
+
+    return hosts
+
+#修改hostname
+def changeHostname():
+    hostname=env.hostmap[env.host]
+#    print hostname
+    sudo('echo \'127.0.0.1 '+hostname+'\' >> /etc/hosts')
+    sudo('echo '+ hostname+' >/etc/hostname' )
+    sudo('hostname '+ hostname)
+
+def downloadJDK():
+    pass
+#    local('wget --no-check-certificate --no-cookies --header Cookie: oraclelicense=accept-securebackup-cookie http://download.oracle.com/otn-pub/java/jdk/8u73-b02/jdk-8u73-linux-x64.tar.gz')
+def distributeJDK():
+    put('jdk1.8.77.tar.gz', 'jdk1.8.77.tar.gz')
+    run('tar -xzf jdk1.8.77.tar.gz')
+    run('echo "export JAVA_HOME=/home/'+user+'/jdk1.8.0_77">~/.bashrc')
+    run('echo "export PATH=$JAVA_HOME/bin:$PATH" >>~/.bashrc')
 
 @roles('client')
+def test3():
+    env.user=cf.get('client','newuser')
+    env.password=cf.get('client','passwd')
+    test2()
+
+def test2():
+        run('ls ./')
+
+@roles('test')
 def test():
     with settings(warn_only=True):
         cd('/')
@@ -64,31 +118,8 @@ def test():
         #if result.failed and not confirm("Tests failed. Continue anyway?"):
         #    abort("Aborting at user request.")
 
-def generateHosts():
-    hosts=''
-    i=0
-    size=len(env.roledefs['server']['hosts'])
-    while i<size:
-        hosts = hosts + '\n' + env.roledefs['server']['ips'][i] + '\t' + env.roledefs['server']['hosts'][i]
-        i=i+1
-    return hosts
-
-
-@roles('server','client')
-def changeHostname():
-    sudo('echo \'127.0.0.1 '+env.host+'\' >> /etc/hosts')
-    sudo('echo '+ env.host+' >/etc/hostname' )
-    sudo('hostname '+ env.host)
-  #  print env.host
-@roles('client')
-def removeClientUser():
-    sudo('deluser '+ cf.get('client','newuser'), pty=False, combine_stderr=False)
-
-@roles('client')
-def test3():
-    env.user=cf.get('client','newuser')
-    env.password=cf.get('client','password')
-    test2()
-
-def test2():
-        run('ls ./')
+def test4():
+    a=cf.get('client','hosts')
+    a=a.split(',')
+    print type(a)
+    print a
--- test.py
+++ test.py
@@ -3,5 +3,6 @@ import ConfigParser
 cf=ConfigParser.ConfigParser()
 cf.read('passwd.ini')
 user=cf.get('server','newuser')
+
 print user
 print user + 'hxd'
