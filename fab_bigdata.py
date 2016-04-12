afbae9fb0536b745f8be877a45b2e99977649bd8
Saint <sainthxd@gmail.com>
2016-04-12 22:03:27 +0800

    add rm cassandra data

--- fab_bigdata.py
+++ fab_bigdata.py
@@ -102,7 +102,7 @@ def runCassandra(status='stop'):
             if(status=='start'):
                 run(os.path.join(cassandra_path,'bin/cassandra')+' -f -p cassandraPID')
             elif(status=='stop'):
-                run('cat cassandraPID| xargs kill')
+                run('cat ' +os.path.join('/home',env.user,'cassandraPID')+'| xargs kill')
             else:
                 print 'unknow command '+ status+", only support start or stop"
 
